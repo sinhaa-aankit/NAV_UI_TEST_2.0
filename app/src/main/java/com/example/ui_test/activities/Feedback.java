e9940eacb0e3deb93b8c6b064c6f96bb728c8ec8
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-05-24 21:15:05 +0530

    Feedback Page Complete

--- app/src/main/java/com/example/ui_test/activities/Feedback.java
+++ app/src/main/java/com/example/ui_test/activities/Feedback.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ui_test.R;
@@ -17,6 +18,8 @@ public class Feedback extends AppCompatActivity {
     }
 
     public void feedback(View view) {
-        Toast.makeText(this,"Not implemented yet" , Toast.LENGTH_SHORT).show();
+        TextView feedback_text = (TextView) findViewById(R.id.feedbackText);
+        feedback_text.setText("");
+        Toast.makeText(this,"ThankYou for your Feedback!" , Toast.LENGTH_SHORT).show();
     }
 }
