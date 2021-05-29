94e0cdc0f23b943109acffab95e1c61685f5901a
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-05-29 20:43:22 +0530

    Feedback to Email

--- app/src/main/java/com/example/ui_test/activities/Feedback.java
+++ app/src/main/java/com/example/ui_test/activities/Feedback.java
@@ -2,6 +2,7 @@ package com.example.ui_test.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -11,6 +12,8 @@ import com.example.ui_test.R;
 
 public class Feedback extends AppCompatActivity {
 
+    Intent intent = new Intent(Intent.ACTION_SEND);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +22,19 @@ public class Feedback extends AppCompatActivity {
 
     public void feedback(View view) {
         TextView feedback_text = (TextView) findViewById(R.id.feedbackText);
+
+        String emailBody = feedback_text.getText().toString();
+
+        intent.setType("message/rfc822");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "some@email.address" });
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+        intent.putExtra(Intent.EXTRA_TEXT, emailBody);
+        intent.setPackage("com.google.android.gm");
+
+        startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+
+
         feedback_text.setText("");
-        Toast.makeText(this,"ThankYou for your Feedback!" , Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this,"ThankYou for your Feedback!" , Toast.LENGTH_SHORT).show();
     }
 }
